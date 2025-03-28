3df28039230630fcad57007c6353cc8d75bbec4e
savokiss <jaynaruto@qq.com>
2025-03-28 17:42:57 +0800

    fix: og props

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,8 @@
 
 export const SiteTitle = 'Flux AI Changelog';
 export const SiteDescription = 'Changelog for https://fluxai.pro';
+export const DefaultOgImage = {
+  src: 'https://s.detools.dev/assets/balg8q301.jpeg',
+  alt: 'Flux AI Changelog'
+};
+export const TwitterHandle = '@FluxAIPro';
